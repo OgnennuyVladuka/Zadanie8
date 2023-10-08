d31692c3db753e12811b66b7d9518993971d148f
OgnennuyVladuka <146809006+OgnennuyVladuka@users.noreply.github.com>
2023-10-08 22:30:15 +0300

    Add files via upload

---
+++ z8/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace z8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количество ступеней:");
+            int vvod = Convert.ToInt32(Console.ReadLine());
+            string[] mas1 = new string[vvod];
+            mas1[0] = "1";
+            Console.WriteLine(mas1[0]);
+            string k = "";
+            for (int i = 1; i < vvod; i++)
+            {
+                k = Convert.ToString(i + 1);
+                mas1[i] = mas1[i - 1] + k;
+                Console.WriteLine(mas1[i]);
+            }
+        }
+    }
+}
